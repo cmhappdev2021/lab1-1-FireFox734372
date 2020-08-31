6c4815292adc8a1951d6baa6e6f47300fb78d305
FireFox734372 <argilbert39@gmail.com>
2020-08-31 18:33:18 +0000

    Added a song and the lead up to the chorus and the chorus

--- main.swift
+++ main.swift
@@ -7,8 +7,24 @@ print("How to use playgrounds to make writing Swift fun and simple")
 
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
 
+// I don't know if I have a favorite song so I'll just pick one from spotify
 
+print("More Than My Hometown")
+print("   Morgan Wallen")
 
 // Use multiple print functions to write out some of the lyrics to the song.
 
-
+print(" ")
+print("...")
+print("But I just need you to know")
+print(" ")
+print("That I love you more than a California sunset")
+print("More than a beer when you ain't twenty-one yet")
+print("More than a Sunday morning Lord")
+print("Turnin' some poor lost souls 'round, Hallelujah bound")
+print("Yeah, I love you more than the feeling when the bass hits a hook")
+print("When the guy gets the girl at the end of the")
+print("book")
+print("But, baby, this might be the last time I get to lay you down")
+print("'Cause I can't love you more than my hometown")
+print("...")
